a27ab2ae71627ec0d4d243964f4e55dee24b55e5
Salman778 <alk-333@hotmail.com>
2019-05-12 00:02:43 +0300

    add comment

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Initial value
 let count = -1, move = 0, events = [], start = 0;
 const cards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-bolt", "fa fa-cube",
                "fa fa-leaf", "fa fa fa-bicycle", "fa fa-bomb", "fa fa-anchor",
@@ -19,11 +20,16 @@ function shuffle(array) {
 
 
 function game() {
+    //Remove all the stars
     document.getElementsByClassName("stars")[0].innerHTML = '';
+    //Add three stars
     for(let i = 0; i < 3; i++)
         document.getElementsByClassName("stars")[0].innerHTML += '<li><i class="fa fa-star"></i></li>';
+    //shuffle our array
     shuffle(cards);
+    //get all our cards
     cardsNumbers = document.querySelectorAll(".card");
+    //add Event Listener for each one from our cards
     for(let i = 0; i < cardsNumbers.length; i++)
         cardsNumbers[i].addEventListener("click", check), cardsNumbers[i].className = "card", 
         cardsNumbers[i].childNodes[1].className = cards[i];
@@ -32,16 +38,22 @@ function game() {
 
 
 function check(){
+    //The timer start
     if(start === 0)
-    start = Date.now();
+        start = Date.now();
+    //Remove Event Listener
     this.removeEventListener("click", check);
+    //flip the card
     this.className = "card open show";
+    //push to the events array to compare with first/second push
     events.push(this);
+    //check if there were two cards flip and if same
     if(events.length === 2 && events[0].childNodes[1].className === events[1].childNodes[1].className){
         events[0].className = "card match", events[1].className = "card match", move++, events = [], count++;
         if(move === 8 || move === 9)
             document.getElementsByClassName("stars")[0].childNodes[0].remove();
     }
+    //check if there were two cards flip and if not same
     else if(events.length === 2)
         setTimeout(function(){
         events[0].className = "card", events[1].className = "card";
@@ -51,8 +63,10 @@ function check(){
         if(move === 7 || move === 9)
             document.getElementsByClassName("stars")[0].childNodes[0].remove();
         }, 200);
+    //if all our cards fliped
     if(count === 7)
         setTimeout(function() {
+            //re-assign Initial value
             start -= Date.now();
             start /= 1000;
             alert("You win\nTime:" + start + " Seconds");
@@ -67,6 +81,7 @@ function check(){
 }
 
 document.getElementsByClassName("restart")[0].addEventListener("click", function(){
+    //re-assign Initial value
     count = -1;
     move = 0;
     start = 0;
